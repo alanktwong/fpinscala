7b1ebce040712c4dac0ed5fb27ff504ced97359c
Alan Wong <alan_kt_wong@yahoo.com>
2015-01-31 11:52:44 -0500

    Work on functional data structures for tree

--- exercises/src/main/scala/fpinscala/datastructures/Tree.scala
+++ exercises/src/main/scala/fpinscala/datastructures/Tree.scala
@@ -7,7 +7,34 @@ case class Branch[A](left: Tree[A], right: Tree[A]) extends Tree[A]
 
 object Tree {
 
+	def size[A](tree: Tree[A]): Int = {
+		???
+	}
 
+	def maximum[Int](tree: Tree[Int]): Tree[Int] = {
+		???
+	}
+	
+	def depth[A](tree: Tree[A]): Int = {
+		???
+	}
+	def map[A,B](tree: Tree[A])(f: A => B): Tree[B] = {
+		???
+	}
 
-
+	def fold[A,B](tree: Tree[A], z: B)(f: (A,B) => B): B = {
+		???
+	}
+	
+	def sizeViaFold[A](tree: Tree[A]): Int = {
+		???
+	}
+	
+	def maximumViaVold(tree: Tree[Int]): Tree[Int] = {
+		???
+	}
+	
+	def mapViaFold[A,B](tree: Tree[A])(f: A => B): Tree[B] = {
+		???
+	}
 }
---
+++ exercises/src/test/scala/fpinscala/datastructures/TreeSpec.scala
@@ -0,0 +1,36 @@
+package fpinscala.datastructures
+
+import fpinscala.AbstractWordSpec
+import org.scalatest.junit.JUnitRunner
+import org.junit.runner.RunWith
+
+
+@RunWith(classOf[JUnitRunner])
+class TreeSpec extends AbstractWordSpec {
+
+	"size" should {
+		"work" in {
+			???
+		}
+	}
+	"maximum" should {
+		"work" in {
+			???
+		}
+	}
+	"depth" should {
+		"work" in {
+			???
+		}
+	}
+	"map" should {
+		"work" in {
+			???
+		}
+	}
+	"fold" should {
+		"work" in {
+			???
+		}
+	}
+}
