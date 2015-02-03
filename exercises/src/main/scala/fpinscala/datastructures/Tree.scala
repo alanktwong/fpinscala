f0a5d0ae8b72b60326cf8071e378f6e88a7d2784
Alan Wong <alan_kt_wong@yahoo.com>
2015-02-02 19:00:23 -0500

    Work on functional data structures for tree

--- exercises/src/main/scala/fpinscala/datastructures/Tree.scala
+++ exercises/src/main/scala/fpinscala/datastructures/Tree.scala
@@ -8,33 +8,74 @@ case class Branch[A](left: Tree[A], right: Tree[A]) extends Tree[A]
 object Tree {
 
 	def size[A](tree: Tree[A]): Int = {
-		???
+		tree match {
+			case Leaf(v) => 1
+			case Branch(l, r) => 1 + size(l) + size(r)
+		}
 	}
 
-	def maximum[Int](tree: Tree[Int]): Tree[Int] = {
-		???
+	def maximum[Int](tree: Tree[Int]):Int = {
+		tree match {
+			case Leaf(v) => v
+			case Branch(l,r) => {
+				// maximum(l) max maximum(r)
+				???
+			}
+		}
 	}
 	
 	def depth[A](tree: Tree[A]): Int = {
+		tree match {
+			case Leaf(v) => 0
+			case Branch(l,r) => 1 + (size(l) max size(r))
+		}
 		???
 	}
 	def map[A,B](tree: Tree[A])(f: A => B): Tree[B] = {
-		???
+		tree match {
+			case Leaf(v) => Leaf(f(v))
+			case Branch(l,r) => Branch(map(l)(f),map(r)(f))
+		}
 	}
 
-	def fold[A,B](tree: Tree[A], z: B)(f: (A,B) => B): B = {
-		???
+	/*
+	 * Like `foldRight` for lists, `fold` receives a "handler" for each of the
+	 * data constructors of the type, and recursively accumulates some value
+	 * using these handlers. As with `foldRight`, `fold(t)(Leaf(_))(Branch(_,_)) == t`,
+	 * and we can use this function to implement just about any recursive function
+	 * that would otherwise be defined by pattern matching.
+ 	 */
+	def fold[A,B](tree: Tree[A])(f: A => B)(g: (B,B) => B): B = {
+		tree match {
+			case Leaf(v) => f(v)
+			case Branch(l,r) => g(fold(l)(f)(g), fold(r)(f)(g))
+		}
 	}
 	
 	def sizeViaFold[A](tree: Tree[A]): Int = {
-		???
+		fold(tree)(a => 1)(1 + _ + _)
 	}
 	
-	def maximumViaVold(tree: Tree[Int]): Tree[Int] = {
-		???
+	def maximumViaVold(tree: Tree[Int]): Int = {
+		fold(tree)(a => a)(_ max _)
 	}
 	
+	/*
+	 * Note the type annotation required on the expression `Leaf(f(a))`.
+	 * 
+	 * This error is an unfortunate consequence of Scala using subtyping
+	 * to encode algebraic data types. Without the annotation, the result
+	 * type of the fold gets inferred as `Leaf[B]` and it is then expected
+	 * that the second argument to `fold` will return `Leaf[B]`, which
+	 * it doesn't (it returns `Branch[B]`). Really, we'd prefer Scala to
+	 * infer `Tree[B]` as the result type in both cases. When working with algebraic data types
+	 * in Scala, it's somewhat common to define helper functions that simply call
+	 * the corresponding data constructors but give the less specific result type
+	 * 
+	 * def leaf[A](a: A): Tree[A] = Leaf(a)
+	 * def branch[A](l: Tree[A], r: Tree[A]): Tree[A] = Branch(l, r)
+	 */
 	def mapViaFold[A,B](tree: Tree[A])(f: A => B): Tree[B] = {
-		???
+		fold(tree)(a => Leaf(f(a)): Tree[B])(Branch(_,_))
 	}
 }
--- exercises/src/test/scala/fpinscala/datastructures/TreeSpec.scala
+++ exercises/src/test/scala/fpinscala/datastructures/TreeSpec.scala
@@ -7,10 +7,13 @@ import org.junit.runner.RunWith
 
 @RunWith(classOf[JUnitRunner])
 class TreeSpec extends AbstractWordSpec {
-
+	val singleton: Tree[Int] = Leaf(1)
+	val branch: Tree[Int] = Branch(Leaf(1), Leaf(2))
+	
 	"size" should {
 		"work" in {
-			???
+			Tree.size(singleton) should be (1)
+			Tree.size(branch) should be (3)
 		}
 	}
 	"maximum" should {
